50b66707caa83c4c304456df8e18ecb154aadffa
shizekang <shizekang@yeah.net>
2018-01-19 11:16:04 +0800

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+import java.math.BigDecimal;
+
+public class Test {
+    public static void main(String[] args) {
+        BigDecimal b1 = new BigDecimal(2.0);
+        BigDecimal b2 = new BigDecimal(3.0);
+        System.out.println(b1);
+
+        System.out.println(b1 == null);
+    }
+}
