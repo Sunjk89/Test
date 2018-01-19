4d4bfbde844f9154a5e6db044a8ed460ef0cd539
shizekang <shizekang@yeah.net>
2018-01-19 11:20:47 +0800

    It's author1

--- src/Test.java
+++ src/Test.java
@@ -5,7 +5,7 @@ public class Test {
         BigDecimal b1 = new BigDecimal(2.0);
         BigDecimal b2 = new BigDecimal(3.0);
         System.out.println(b1);
-
         System.out.println(b1 == null);
+        System.out.println("It's author1");
     }
 }
